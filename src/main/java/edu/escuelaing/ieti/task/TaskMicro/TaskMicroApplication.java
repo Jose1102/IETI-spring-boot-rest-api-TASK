1525c634c359293be470c2e29d6dadd6120f829d
Jose1102 <jose.castroortega1102@gmail.com>
2021-08-25 20:53:41 -0500

    first commit

---
+++ src/main/java/edu/escuelaing/ieti/task/TaskMicro/TaskMicroApplication.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.ieti.task.TaskMicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMicroApplication.class, args);
+	}
+
+}
